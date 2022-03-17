c5522e9fb5828a226598c5beba688d8b7b8558f8
Novaii-Yoder <52936757+Novaii-Yoder@users.noreply.github.com>
2022-03-17 13:40:06 -0600

    Competed Lab6

--- Lab6/src/RandomSentences.java
+++ Lab6/src/RandomSentences.java
@@ -4,21 +4,30 @@ public class RandomSentences {
     public static void main (String args[]) {
 
         Scanner scan = new Scanner(System.in);
-        String nouns[] = new String[10];
-        String verbs[] = new String[10];
+        LinkedBag nouns = new LinkedBag<String>();
+        LinkedBag verbs = new LinkedBag<String>();
 
+        // gets input and cleans it before putting them into bags
         System.out.println("Input 10 nouns:");
-        for (int i = 0; i < 10; i++)
-            nouns[i] = scan.next();
-
-        System.out.println("Input 10 verbs:");
-        for (int i = 0; i < 10; i++)
-            verbs[i] = scan.next();
-
-        // cleans the input
         for (int i = 0; i < 10; i++) {
-            
+            String s = scan.next();
+        s = s.replaceAll("[^a-zA-Z]", "");
+            s = s.toLowerCase();
+            nouns.add(s);
+        } // end of for
+        System.out.println("Input 10 verbs:");
+        for (int i = 0; i < 10; i++) {
+            String s = scan.next();
+            s = s.replaceAll("[^a-zA-Z]", "");
+            s = s.toLowerCase();
+            verbs.add(s);
+        } // end of for
 
+        System.out.println("How many sentences do want to generate?");
+        int N = scan.nextInt();
+
+        for(int i = 0; i < N; i++) {
+            System.out.println("The " + nouns.grab() + " " + verbs.grab() + " the " + nouns.grab() + ".");
         } // end of for
 
     } // end of main

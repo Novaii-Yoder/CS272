2f97e02492d1f5f24b5c26edfcc7e4d8a9580dab
Novaii-Yoder <52936757+Novaii-Yoder@users.noreply.github.com>
2022-03-17 12:28:21 -0600

    Update to RandomSentences

--- Lab6/src/RandomSentences.java
+++ Lab6/src/RandomSentences.java
@@ -1,3 +1,25 @@
-public class RandomSentences {
+import java.util.Scanner;
 
+public class RandomSentences {
+    public static void main (String args[]) {
+
+        Scanner scan = new Scanner(System.in);
+        String nouns[] = new String[10];
+        String verbs[] = new String[10];
+
+        System.out.println("Input 10 nouns:");
+        for (int i = 0; i < 10; i++)
+            nouns[i] = scan.next();
+
+        System.out.println("Input 10 verbs:");
+        for (int i = 0; i < 10; i++)
+            verbs[i] = scan.next();
+
+        // cleans the input
+        for (int i = 0; i < 10; i++) {
+            
+
+        } // end of for
+
+    } // end of main
 }
